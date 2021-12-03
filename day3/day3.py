22543d7f580fed067ff7d836753794bedabc2169
Dalton McPhaden <dmcphade@uwo.ca>
2021-12-03 16:56:31 -0500

    day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -2,7 +2,6 @@ with open("day3/input.txt") as file:
     input = file.read().splitlines()
 
 print("Day 2 Part 1:")
-size = len(input[0])
 counts = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 for number in input:
     for digit in range(len(number)):
@@ -40,3 +39,74 @@ print(gamma_rate_decimal)
 print(epsilon_rate_decimal)
 
 print(gamma_rate_decimal * epsilon_rate_decimal)
+
+print("Day 2 Part 2:")
+
+import numpy as np
+
+zeroesCount = 0
+onesCount = 0
+zeroesList = []
+onesList = []
+
+oxygenList = input
+
+for i in range(len(input[0])):
+
+    for number in oxygenList:
+        if (number[i] == '0'):
+            zeroesCount += 1
+            zeroesList.append(number)
+        else:
+            onesCount += 1
+            onesList.append(number)
+
+    if zeroesCount > onesCount:
+        oxygenList = np.intersect1d(oxygenList, zeroesList)
+    else:
+        oxygenList = np.intersect1d(oxygenList, onesList)
+    
+    zeroesCount = 0
+    onesCount = 0
+    zeroesList = []
+    onesList = []
+
+print(oxygenList)
+
+zeroesCount = 0
+onesCount = 0
+zeroesList = []
+onesList = []
+
+co2List = input
+
+for i in range(len(input[0])):
+    if len(co2List) == 1:
+        break
+
+    for number in co2List:
+        if (number[i] == '0'):
+            zeroesCount += 1
+            zeroesList.append(number)
+        else:
+            onesCount += 1
+            onesList.append(number)
+
+    if zeroesCount <= onesCount:
+        co2List = np.intersect1d(co2List, zeroesList)
+    else:
+        co2List = np.intersect1d(co2List, onesList)
+    
+    zeroesCount = 0
+    onesCount = 0
+    zeroesList = []
+    onesList = []
+
+print(co2List)
+
+oxygen_decimal = int(oxygenList[0], 2)
+
+co2_decimal = int(co2List[0], 2)
+
+
+print(oxygen_decimal * co2_decimal)
